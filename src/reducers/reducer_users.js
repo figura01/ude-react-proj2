4a97924130088387ccd75cc156f52deebe08bec5
laurent vuillaume <figura.graphik@gmail.com>
2019-05-06 16:51:31 +0200

    up react proj

---
+++ src/reducers/reducer_users.js
@@ -0,0 +1,8 @@
+export default () => {
+    return [
+        {name:'Sarah Coche',id:1,role:'ROOT',active:'NON'},
+        {name:'Eleonore Labanquise',id:2,role:'USER',active:'OUI'},
+        {name:'Emma Karena',id:3,role:'USER',active:'NON'},
+        {name:'Jean Bonnot',id:4,role:'ADMIN',active:'OUI'},
+    ]
+}
